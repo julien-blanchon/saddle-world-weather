60cbdbf9fea65de1fd910f7a3b7e1d9d7820709e
Julien Blanchon <julien@blanchon.cc>
2026-04-02 16:00:00 +0100

    Fix CI: use correct crate name saddle_world_weather in integration test

--- tests/public_api.rs
+++ tests/public_api.rs
@@ -1,5 +1,5 @@
 use bevy::{asset::AssetPlugin, prelude::*, transform::TransformPlugin};
-use weather::{WeatherPlugin, WeatherSystems};
+use saddle_world_weather::{WeatherPlugin, WeatherSystems};
 
 #[test]
 fn public_plugin_and_sets_are_usable() {
@@ -16,5 +16,5 @@ fn public_plugin_and_sets_are_usable() {
     app.add_systems(Update, (|| {}).after(WeatherSystems::Diagnostics));
     app.update();
 
-    assert!(app.world().contains_resource::<weather::WeatherRuntime>());
+    assert!(app.world().contains_resource::<saddle_world_weather::WeatherRuntime>());
 }
